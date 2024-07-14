dcbf114e76d0ba9458f23215636369a5a056dae7
CRAZy-Monk3Y <tathagata.chakraborty97@gmail.com>
2024-07-14 20:52:31 +0530

    Day 1: done

---
+++ day1/index.js
@@ -0,0 +1,25 @@
+// Adctivities
+var num1 = 10;
+let num2 = 20;
+
+console.log(num1);
+console.log(num2);
+
+const booleanVal = false;
+
+console.log(booleanVal);
+
+let string1 = "My Name is Tathagata";
+let User = {
+  name: "John",
+  email: "john@email.com",
+};
+var arr1 = [1, 2, 3, 4, 5, 6, 7];
+// Get Types
+console.log(typeof num1);
+console.log(typeof string1);
+console.log(typeof booleanVal);
+console.log(typeof User);
+console.log(typeof arr1);
+
+booleanVal = true;  // ERROR:
